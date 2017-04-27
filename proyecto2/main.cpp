14a5ef6050f17e0c1caabdb62cc6d5567687d911
alexa1999 <anel.veliz@ucsp.edu.pe>
2017-04-27 14:47:55 -0500

    Add files via upload

---
+++ proyecto2/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+
+/*int main()
+{
+    float x;
+    float p =3.14;
+    cout << "ingrese radio del circulo:" << endl;
+    cin >> x;
+    cout << "el area del circulo es :"<< p*x*x<< endl;
+
+    int c;
+    cout << " ingrese el del cuadrado:" << endl ;
+    cin >> c;
+    cout << "el area del cuadrado :" << c*c endl ;
+
+    int r;
+    cout << " ingrese la base del rectangulo:" << endl ;
+    cin >> c;
+    int a;
+    cout << "ingrese la altura del rectangulo :" <<  endl ;
+    cin >> a;
+    cout << " el area del rectangulo es :" << c*a << endl;
+
+    return 0;
+
+}*/
+/*int main()
+{
+    int x =10;
+    int y = 20;
+    int temp;
+    temp = x;
+    x = y;
+    y = temp;
+    cout << "x es igual :" << x << endl ;
+    cout << " y es igual :" << y << endl;
+    return 0;
+}*/
+/*int main()
+{
+    int x = 10;
+    int y = 20;
+    int a=a+b;
+    int b = a-b;
+    int a = a-b;
+}*/
+
+/*int main()
+{
+    int x;
+    cout << "ingrese un numero para x:" << endl;
+    cin >> x;
+    int y;
+    cout << "ingrese un numero para y:"<<  endl;
+    cin >> y;
+    if (x>y){
+        cout << x << "es mayor que " << y << endl;
+        }
+        else {
+            cout<< y<<" es mayor que" << x<< endl;
+
+        }
+    return 0;
+
+}*/
+
+int main()
+{
+    int c =5;
+    int x;
+    x=c++ ;
+    cout << " x es igual :"<< x << endl;
+    return 0;
+}
